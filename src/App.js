42caafd58398b1a45291ef4947219dcf37cac324
mohit-thakre <mohitthakre010@gmail.com>
2024-07-12 20:05:56 +0530

    responsiveness

--- src/App.js
+++ src/App.js
@@ -14,20 +14,20 @@ function App() {
 
   return (
     <div
-      className="layer w-full h-screen flex justify-center items-center"
+      className="layer w-full min-h-screen md:h-screen flex flex-col md:flex-row  justify-center items-center"
       style={{
         backgroundImage: `linear-gradient(196deg, ${data[index].background_image1}, ${data[index].background_image2})`,
       }}
     >
-      <div className="w-1/3">
+      <div className=" w-full pt-10 md:pt-0 md:w-1/3">
         <img src={data[index].image} alt="Chair" className="ab"></img>
       </div>
-      <div className="w-1/3">
+      <div className=" w-full md:w-1/3 flex flex-col justify-center items-center">
         <h5 className="font-Poppins font-bold text-white py-2">MODERN CHAIR</h5>
         <h1 className="font-Poppins font-bold text-4xl text-white py-2">
           {data[index].name}
         </h1>
-        <h3 className="font-Poppins font-bold text-3xl text-white py-2">
+        <h3 className="font-Poppins font-bold text-3xl text-white py-2 flex gap-4 justify-center items-center">
           ${data[index].chair_price}{" "}
           <span className="text-gray-300 line-through text-xl">$530</span>
         </h3>
@@ -58,7 +58,7 @@ function App() {
         <p className="font-Poppins font-semibold text-lg text-white py-2">
           choose color:
         </p>
-        <div className="flex justify-start items-center ">
+        <div className="flex justify-center md:justify-start items-center ">
           {data.map((item, idx) => (
             <>
               <img
@@ -75,7 +75,7 @@ function App() {
         </div>
 
         <button
-          className=" px-24 my-4 py-4  rounded-lg text-white font-bold flex  items-center hover:bg-slate-400 "
+          className="  px-24 my-4 py-2 md:py-4  rounded-lg text-white font-bold flex justify-center  items-center hover:bg-slate-400 "
           style={{
             backgroundColor: hover ? "#5a5a5b" : data[index].button,
             transition: "0.5s",
@@ -83,8 +83,8 @@ function App() {
           onMouseEnter={() => setHover(true)}
           onMouseLeave={() => setHover(false)}
         >
-          <MdOutlineShoppingCart className="mx-2 text-xl font-extrabold" /> Add
-          To Cart
+          <MdOutlineShoppingCart className="mx-2 text-xl font-extrabold" />
+          <span> Add To Cart</span>
         </button>
       </div>
     </div>
